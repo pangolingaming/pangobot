324b325218fb77922162b32954abc636618d3162
Mirri <discworldian@gmail.com>
2020-12-23 15:15:07 +0000

    bot 0.1

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.TOKEN);
