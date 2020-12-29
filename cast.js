5149a08a4d5158f1c30c66fa7164d6b0d567d7ae
Mirri <discworldian@gmail.com>
2020-12-29 17:23:04 +0000

    Additional spell functionality

--- cast.js
+++ cast.js
@@ -5,6 +5,8 @@ const yaml = require("js-yaml");
 const spellsData = fs.readFileSync("./content/spells.yaml", "utf8");
 const spellsList = yaml.safeLoad(spellsData);
 
+const damageIcons = { acid: ":lemon:", fire: ":fire:" };
+
 function roll(rollString) {
   var diceNumber = rollString.split("d")[0];
   var diceSides = rollString.split("d")[1];
@@ -27,28 +29,31 @@ function roll(rollString) {
 function cast(msg) {
   const args = msg.content.slice("!cast".length).trim().split(" ");
 
-  var spellNameFiltered = args[0].toLowerCase();
-  var spell = spellsList[spellNameFiltered];
+  const spell = spellsList[args[0].toLowerCase()];
 
   /* Figure out whether spell is upcast or not - mostly for damage purposes. */
-
-  var spellCastLevel = args[1];
+  let spellCastLevel;
+  if (Number.isInteger(parseInt(args[1])) === false) {
+    spellCastLevel = spell.spellLevel;
+  } else {
+    spellCastLevel = parseInt(args[1]);
+  }
   var spellBaseLevel = spell.spellLevel;
   var upCast = false;
   var upCastBy = 0;
 
-    /* Is it upcast, and if so, by how much? */ 
+  /* Is it upcast, and if so, by how much? */
   if (
-    (spellCastLevel !== undefined) &&
-    (parseInt(spellCastLevel) > parseInt(spellBaseLevel)) &&
-    (parseInt(spellCastLevel) <= 9)
+    spellCastLevel !== undefined &&
+    parseInt(spellCastLevel) > parseInt(spellBaseLevel) &&
+    parseInt(spellCastLevel) <= 9
   ) {
     upCast = true;
     upCastBy = parseInt(spellCastLevel) - parseInt(spellBaseLevel);
   }
 
   /* Roll for damage */
-
+  if (spell.damage) {
   var damageRoll = spell.damage;
 
   if (upCast === true) {
@@ -63,7 +68,7 @@ function cast(msg) {
     damageRoll = upCastDiceNumber + "d" + spellDamageDiceType;
   }
 
-  var damage = roll(damageRoll);
+  var damage = roll(damageRoll)}
 
   /* Footer */
 
@@ -74,25 +79,63 @@ function cast(msg) {
   }
 
   const castEmbed = new Discord.MessageEmbed()
-    .setTitle(`${spell.spellName}`)
+    .setTitle(
+      `${damageIcons[spell.damageType] ? damageIcons[spell.damageType] : ""} ${
+        spell.spellName
+      } ${damageIcons[spell.damageType] ? damageIcons[spell.damageType] : ""}`
+    )
     .addFields(
-      { name: "Spell level:", value: `${spell.spellLevel}`, inline: true },
-      { name: "Upcast:", value: `${upCast}`, inline: true }
+      { name: "Spell school:", value: `${spell.spellSchool}`, inline: true },
+      {
+        name: "Spell level:",
+        value: `${spell.spellLevel === "0" ? "cantrip" : spell.spellLevel}`,
+        inline: true,
+      }
     )
     .setFooter(`${footer}`);
 
+  /* Add upcast field */
+
+  if (upCast) {
+    castEmbed.fields.push({
+      name: "Upcast:",
+      value: `Cast with spell level **${spellCastLevel}**, upcast by **${upCastBy}**.`,
+      inline: true,
+    });
+  } else if (spell.spellLevel !== "0") {
+    castEmbed.fields.push({ name: "Upcast:", value: `No`, inline: true });
+  }
+
+  /* Add description field (also if spell doesn't deal damage) */
+
+  if (args.includes("desc") || spell.damage === undefined) {
+    castEmbed.fields.push({
+      name: "Spell description:",
+      value: spell.description,
+    });
+  }
+
+  /* Add upcast description if spell doesn't deal damage and it is upcast */
+
+  if (spell.damage === undefined && upCast && spell.atHigherLevels) {
+    castEmbed.fields.push({
+      name: "At Higher Levels:", value: spell.atHigherLevels
+    })
+  }
+
   /* Add damage inline field */
 
-  if (spell.damage !== undefined) {
+  if (spell.damage) {
     castEmbed.fields.push({
       name: "Result",
-      value: `**${damage.diceTotal}** ${
-        spell.damageType
-      } damage - ${damage.rollString} rolled (${damage.diceRoll.toString()}).`,
+      value: `**${damage.diceTotal}** ${spell.damageType} damage - ${
+        damage.rollString
+      } rolled (${damage.diceRoll.toString()}).`,
     });
 
-    msg.reply(castEmbed);
+    
   }
+  msg.reply(castEmbed);
 }
 
 exports.cast = cast;
