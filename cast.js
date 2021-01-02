8f87d2c43e268b2665ad5497e75da28f60e710c5
Mirri <discworldian@gmail.com>
2021-01-02 16:04:24 +0000

    Cleaned up functions and added the "short" variable

--- cast.js
+++ cast.js
@@ -99,29 +99,51 @@ const dndDamageTypes = [
 // Cantrip progression - beams (Eldritch Blast)
 // Magic missile - beams
 // Scorching Ray - beams
-// Flamestrike - two types of damage
-// Ice Storm - same thing
-// Meteor Swarm - same thing
-// Wall of Ice - same thing
 // Combine these two in array?
 //
 // Combine in array - type, damage, athigherlevels for each damage
 
-/* function to account for more than one damage type option */
+/* Function to split the string into an array, removing "!cast" */
 
-function joinArrayWithCommas(damageTypeString, damageTypes) {
+function splitString(msg) {
+  return msg.content.slice("!cast".length).trim().split(" ");
+}
+
+/* Set spell type icons and create title. First spell type determines the icon in the title. */
+
+function setTitle(spell) {
+  var typeIcon = typeIcons[spell.spellType[0]] || "";
+  const title = `${typeIcon} ${spell.spellName} ${typeIcon}`;
+  return title;
+}
+
+/* Function to account for more than one damage type option */
+
+function joinArrayWithCommas(damageTypes) {
+  let damageTypeString
   if (damageTypes.length > 1) {
-    let comma = "";
-    if (damageTypes.length > 2) {
-      comma = ",";
-    }
+    let comma = (damageTypes.length > 2) ? "," : "";
     damageTypeString =
       damageTypes.slice(0, damageTypes.length - 1).join(", ") +
       comma +
       " or " +
       damageTypes[damageTypes.length - 1];
-  } else damageTypeString = damageTypes[0];
+      return damageTypeString
+  } else return damageTypes[0]
 }
+
+  /* If there are multiple damage types, add them all to an array, then join that array to make a comma separated string. */
+
+  function addSpellDamageTypesToArray(spell) {
+    let damageTypes = [];
+    for (i = 0; i < spell.spellType.length; i++) {
+      if (dndDamageTypes.includes(spell.spellType[i])) {
+        damageTypes.push(spell.spellType[i]);
+      }
+    }
+    return damageTypes;
+  }
+
 
 /* To calculate the dice from a roll string */
 
@@ -164,40 +186,51 @@ function rollMultipleAttacks(attacks, rollString) {
   return attacksArray;
 }
 
-function cast(msg) {
-  const args = msg.content.slice("!cast".length).trim().split(" ");
+/* Variables for upcasting and attack mod purposes. */
+var upCast = false;
+var upCastBy = 0;
+let spellCastLevel;
+let spellAttackMod;
+let charLevel;
+let short;
 
+function cast(msg) {
+
+  const args = splitString(msg);
   const spell = spellsList[args[0].toLowerCase()];
 
-  let spellCastLevel;
-  let spellAttackMod;
-  let charLevel;
+  const spellBaseLevel = parseInt(spell.spellLevel);
+
+  /* Check for concentration and add a C symbol - this will be used in the duration field later */
+
+  const concentration = spell.concentration ? ":regional_indicator_c: " : "";
+
+  if (spell === undefined) {
+    return msg.reply(
+      "I can't find this spell. Did you add underscores into the spell name?"
+    );
+  }
 
   args.slice(1).forEach((arg) => {
     if (arg[0].match(/[Ll]/)) {
       spellCastLevel = parseInt(arg.substring(1));
     } else if (arg[0].match(/[+-]/)) {
       spellAttackMod = parseInt(arg);
+    } else if(arg.match(/ *short\ */)) {
+      short = true
     }
+    
   });
-
-  /* Check for concentration - this will be used in duration later */
-
-  const concentration = spell.concentration ? ":regional_indicator_c: " : "";
 
   /* Figure out whether spell is upcast or not - mostly for damage purposes. */
 
   if (!Number.isInteger(spellCastLevel)) {
     spellCastLevel = spell.spellLevel;
   }
-  const spellBaseLevel = parseInt(spell.spellLevel);
-
-  var upCast = false;
-  var upCastBy = 0;
 
   /* Is it upcast, and if so, by how much? */
   if (
-    spellCastLevel !== undefined &&
+    spellCastLevel &&
     spellCastLevel > spellBaseLevel &&
     spellCastLevel <= 9
   ) {
@@ -228,44 +261,36 @@ function cast(msg) {
 
   /* Footer */
 
-  var footer = "PangoBot is a good bot.";
+  var footer =
+    spell.source === "SRD"
+      ? `Rules used under the terms of the OGL 1.0a.`
+      : "PangoBot is a good bot.";
 
-  if (spell.source === "SRD") {
-    footer = "Rules used under the terms of the OGL 1.0a.";
-  }
-
-  /* Set spell type and damage type(s). First spell type determines the icon in the title. */
-
-  var typeIcon = typeIcons[spell.spellType[0]] || "";
-  let damageTypes = [];
-  for (i = 0; i < spell.spellType.length; i++) {
-    if (dndDamageTypes.includes(spell.spellType[i])) {
-      damageTypes.push(spell.spellType[i]);
-    }
-  }
-  let damageTypeString;
-
-  joinArrayWithCommas(damageTypeString, damageTypes);
+  var damageTypes = joinArrayWithCommas(addSpellDamageTypesToArray(spell));
 
   const castEmbed = new Discord.MessageEmbed()
-    .setTitle(`${typeIcon} ${spell.spellName} ${typeIcon}`)
-    .addFields(
-      {
-        name: "Spell level:",
-        value: `${spell.spellLevel === "0" ? "cantrip" : spell.spellLevel}`,
-        inline: true,
-      },
-      { name: "Spell school:", value: `${spell.spellSchool}`, inline: true },
-      {
-        name: "Casting time:",
-        value: `${spell.castingTime}  ${spell.ritual ? "(ritual)" : ""}`,
-        inline: true,
-      },
-      { name: "Range:", value: `${spell.range}`, inline: true }
-    )
+    .setTitle(`${setTitle(spell)}`)
+    
     .setFooter(`${footer}`);
 
-  /* Add upcast field */
+  /* Check for short messages, else add more fields */
+  if (!short) {
+    castEmbed.fields.push(
+    {
+      name: "Spell level:",
+      value: `${spell.spellLevel === "0" ? "cantrip" : spell.spellLevel}`,
+      inline: true,
+    },
+    { name: "Spell school:", value: `${spell.spellSchool}`, inline: true },
+    {
+      name: "Casting time:",
+      value: `${spell.castingTime}  ${spell.ritual ? "(ritual)" : ""}`,
+      inline: true,
+    },
+    { name: "Range:", value: `${spell.range}`, inline: true }
+  )}
+
+  /* Add upcast field only if the spell can be upcast. */
 
   if (upCast) {
     castEmbed.fields.push({
@@ -273,11 +298,11 @@ function cast(msg) {
       value: `Cast with spell level **${spellCastLevel}**, upcast by **${upCastBy}**.`,
       inline: true,
     });
-  } else if (spell.spellLevel !== "0") {
+  } else if (spell.spellLevel !== "0" && spell.atHigherLevels && !short) {
     castEmbed.fields.push({ name: "Upcast:", value: `No`, inline: true });
   }
 
-  /* Add duration and concentration if not instantaneous */
+  /* Add duration and concentration if not instantaneous. */
 
   if (spell.duration !== "Instantaneous") {
     castEmbed.fields.push({
@@ -288,6 +313,7 @@ function cast(msg) {
   }
 
   /* Add description field (also if spell doesn't deal damage) */
+
   var spellDescription = spell.description;
   if (spellDescription.length > 1000) {
     spellDescription = spellDescription.substring(0, 1000) + " ...";
@@ -301,7 +327,7 @@ function cast(msg) {
 
   /* Add upcast description if spell doesn't deal damage and it is upcast */
 
-  if (spell.damage === undefined && upCast && spell.atHigherLevels) {
+  if ((! spell.damage && ! spell.damageArray) && upCast && spell.atHigherLevels) {
     castEmbed.fields.push({
       name: "At Higher Levels:",
       value: spell.atHigherLevels,
@@ -337,27 +363,13 @@ function cast(msg) {
 
   let spellRollText = spell.spellRollText || "damage";
 
-  // If there's multiple options for damage, split them with "or". Going for an Oxford comma in this list.
-
-  if (damageTypes.length > 1) {
-    let comma = "";
-    if (damageTypes.length > 2) {
-      comma = ",";
-    }
-    damageTypeString =
-      damageTypes.slice(0, damageTypes.length - 1).join(", ") +
-      comma +
-      " or " +
-      damageTypes[damageTypes.length - 1];
-  } else damageTypeString = damageTypes[0];
-
   /* Add the damage string(s) to the message */
 
   if (spell.damage) {
     castEmbed.fields.push({
       name: "Result",
       value: `**${damageTotal}** ${
-        damageTypeString === undefined ? "" : damageTypeString
+        damageTypes === undefined ? "" : damageTypes
       } ${spellRollText}${
         damage.rollString === "0d0" ? "" : " - " + damage.rollString + " "
       }${
@@ -366,7 +378,11 @@ function cast(msg) {
           : "rolled (" + damage.diceRoll.toString() + ")"
       }${spellAttackMod ? " +  " + spellAttackMod : ""}${
         plusTotal && damage.rollString !== "0d0" ? " + " + plusTotal : ""
-      }.${spell.spellName === 'Vampiric Touch' ? ' Regain **' + (Math.floor(damageTotal/2)) + '** hp.' : ''}`,
+      }.${
+        spell.spellName === "Vampiric Touch"
+          ? " Regain **" + Math.floor(damageTotal / 2) + "** hp."
+          : ""
+      }`,
     });
   }
   msg.reply(castEmbed);
